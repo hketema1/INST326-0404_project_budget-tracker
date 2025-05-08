ad2e7b3a57343a51b0b4e74ea94c4fb622318ee0
hketema1 <hawareyaweketema@gmail.com>
2025-05-08 17:57:21 -0400

    Update test_transaction.py
    Docstrings added

--- test_transaction.py
+++ test_transaction.py
@@ -2,13 +2,23 @@
 test_transaction.py
 
 Unit tests for the Transaction class.
+
 """
 
 import unittest
 from transaction import Transaction
 
 class TestTransaction(unittest.TestCase):
+    
+    """ 
+    These are unit tests for methods in the transaction class
+    """
+    
     def test_transaction_to_dict(self):
+        """ 
+        This tests that to_dict() correctly converts a transaction to dictionary
+        """
+        
         t = Transaction(100.0, "Food", "Lunch", "expense")
         expected = {
             "amount": 100.0,
@@ -19,6 +29,11 @@ class TestTransaction(unittest.TestCase):
         self.assertEqual(t.to_dict(), expected)
 
     def test_transaction_from_dict(self):
+        
+        """ 
+        This tests from_dict() correctly creates a Transaction object from a dictionary
+        """
+        
         data = {
             "amount": 50.0,
             "category": "Salary",
